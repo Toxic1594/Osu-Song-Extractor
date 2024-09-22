e5dbcabc18edbba1dfc1432bf38060e1194648d8
Dust981337 <Dust981337@gmail.com>
2024-09-22 14:14:15 +0200

    Projektdateien hinzufügen.

---
+++ osu song extractor/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace osu_song_extractor
+{
+    partial class f1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbexfolder = new System.Windows.Forms.CheckBox();
+            this.cbexfile = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(102)))), ((int)(((byte)(170)))));
+            this.btnSelectFolder.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(51)))), ((int)(((byte)(119)))));
+            this.btnSelectFolder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(45)))), ((int)(((byte)(90)))));
+            this.btnSelectFolder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(187)))));
+            this.btnSelectFolder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectFolder.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnSelectFolder.Location = new System.Drawing.Point(3, 25);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(233, 27);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "Select Osu Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = false;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(102)))), ((int)(((byte)(170)))));
+            this.label1.Location = new System.Drawing.Point(-1, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Osu Song Extractor by Toxic1594";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbexfolder
+            // 
+            this.cbexfolder.AutoSize = true;
+            this.cbexfolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.cbexfolder.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(187)))));
+            this.cbexfolder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(45)))), ((int)(((byte)(90)))));
+            this.cbexfolder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(187)))));
+            this.cbexfolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(102)))), ((int)(((byte)(170)))));
+            this.cbexfolder.Location = new System.Drawing.Point(3, 83);
+            this.cbexfolder.Name = "cbexfolder";
+            this.cbexfolder.Size = new System.Drawing.Size(112, 19);
+            this.cbexfolder.TabIndex = 3;
+            this.cbexfolder.Text = "Extract as Folder";
+            this.cbexfolder.UseVisualStyleBackColor = false;
+            // 
+            // cbexfile
+            // 
+            this.cbexfile.AutoSize = true;
+            this.cbexfile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.cbexfile.Checked = true;
+            this.cbexfile.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbexfile.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(187)))));
+            this.cbexfile.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(45)))), ((int)(((byte)(90)))));
+            this.cbexfile.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(153)))), ((int)(((byte)(187)))));
+            this.cbexfile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(102)))), ((int)(((byte)(170)))));
+            this.cbexfile.Location = new System.Drawing.Point(3, 68);
+            this.cbexfile.Name = "cbexfile";
+            this.cbexfile.Size = new System.Drawing.Size(103, 19);
+            this.cbexfile.TabIndex = 4;
+            this.cbexfile.Text = "Extract as Mp3";
+            this.cbexfile.UseVisualStyleBackColor = false;
+            // 
+            // f1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(46)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(240, 206);
+            this.Controls.Add(this.cbexfile);
+            this.Controls.Add(this.cbexfolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "f1";
+            this.ShowIcon = false;
+            this.Text = "Osu Song extractor";
+            this.Load += new System.EventHandler(this.f1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.f1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.f1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.f1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbexfolder;
+        private System.Windows.Forms.CheckBox cbexfile;
+    }
+}
+
---
+++ osu song extractor/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace osu_song_extractor
+{
+    public partial class f1 : Form
+    {
+        #region Form dragging
+        private bool dragging = false;
+        private Point dragCursorPoint;
+        private Point dragFormPoint;
+
+
+        private void f1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dragging = true;
+                dragCursorPoint = Cursor.Position;
+                dragFormPoint = this.Location;
+            }
+        }
+        private void f1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                // Unterschied zur Cursorposition berechnen
+                int deltaX = Cursor.Position.X - dragCursorPoint.X;
+                int deltaY = Cursor.Position.Y - dragCursorPoint.Y;
+
+                // Neue Position berechnen
+                this.Location = new Point(dragFormPoint.X + deltaX, dragFormPoint.Y + deltaY);
+            }
+        }
+        private void f1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+        #endregion
+        int counter;
+        string outputfolder;
+        string Osufolder;
+        string[] folderlist;
+        List<string> forbidden = new List<string>() {"easy1.mp3","easy2.mp3","easy3.mp","easy4.mp3", "easy5.mp3", "sectionfail.mp3", "sectionpass.mp3" };
+        public f1()
+        {
+            InitializeComponent();
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\osu!"))
+            {
+                Osufolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\osu!";
+            }
+            else
+            {
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Toxic1594");
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+
+            using (FolderBrowserDialog Folddiag = new FolderBrowserDialog())
+            {
+                Folddiag.SelectedPath = Osufolder;
+                if (Folddiag.ShowDialog() == DialogResult.OK)
+                {
+                    if (Folddiag.SelectedPath.ToLower().Contains("osu"))
+                    {
+                        if (cbexfolder.Checked)
+                        {
+                            MessageBox.Show("Now select the output folder");
+                            FolderBrowserDialog fs = new FolderBrowserDialog();
+                            if (fs.ShowDialog() == DialogResult.OK)
+                            {
+                                outputfolder = fs.SelectedPath;
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                        if (cbexfile.Checked)
+                        {
+                            MessageBox.Show("Now select the output folder");
+                            FolderBrowserDialog fs2 = new FolderBrowserDialog();
+                            if (fs2.ShowDialog() == DialogResult.OK)
+                            {
+                                outputfolder = fs2.SelectedPath;
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                        btnSelectFolder.Text = "Extract";
+                        string Folderpath;
+                        Folderpath = Folddiag.SelectedPath+"\\Songs";
+                        folderlist = Directory.GetDirectories(Folderpath);
+                        foreach (var x in folderlist)
+                        {
+                            string[] files = Directory.GetFiles(x, "*.mp3");
+                            
+                            foreach(var y in files)
+                            {
+                                if (forbidden.Any(word => y.Contains(word)))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    counter++;
+                                    string[] parts = y.Split('\\');
+
+                                    if (parts.Length >= 2) // Überprüfen, ob genug Teile vorhanden sind
+                                    {
+
+                                        string filename = parts[parts.Length - 1];
+                                        string newfolder = parts[parts.Length - 2];
+                                        if (cbexfolder.Checked)
+                                        {
+                                            if (!Directory.Exists(outputfolder+"\\"+ newfolder))
+                                            {
+                                                Directory.CreateDirectory(outputfolder + "\\" + newfolder);
+                                            }
+                                            
+                                            File.Copy(y, outputfolder + "\\" + newfolder);
+                                        }
+                                        if (cbexfile.Checked)
+                                        {
+                                            File.Copy(y, outputfolder + "\\" + newfolder + ".mp3");
+                                        }
+
+                                    }
+                                }
+                            }
+
+
+                        }
+                        MessageBox.Show(counter.ToString()+" Files extracted");
+                    }
+                    else
+                    {
+                        MessageBox.Show("You didnt selected Osu");
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void f1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
